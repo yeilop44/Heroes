4094eef4b65e1356125ddcaa33a2ff166f8b2462
yeilop44 <pc_ylopez@hotmail.com>
2018-09-05 11:33:04 -0500

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { BuscadorComponent } from './components/buscador/buscador.component';
 
 
 const app_routes: Routes = [
-	{ path: 'home', component: HomeComponent },
+	//{ path: 'home', component: HomeComponent },
 	{ path: 'about', component: AboutComponent },
 	{ path: 'heroes', component: HeroesComponent },
 	{ path: 'heroe/:id', component: HeroeComponent },
