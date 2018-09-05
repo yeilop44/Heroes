3293175b04e836b5f69fb2b265c40f344e855148
yeilop44 <pc_ylopez@hotmail.com>
2018-09-05 11:15:25 -0500

    commit

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express= require('express'),
-	 path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-const app =express();
+const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + './dist/heroes'));
 
-app.use(express.static('./dist/heroes'));
-
-app.get('/*', (req,res)=>{
+app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/heroes/index.html'));
-
+res.sendFile(path.join(__dirname+'/dist/heroes/index.html'));
 });
 
-app.listen(process.env.PORT || 8080, ()=>{
-console.log('Server started');
-})
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
